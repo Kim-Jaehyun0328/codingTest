0312145c1a6ad8312916d3ab7ece067f3f5b9413
KimJaehyun <rlawwogus@ajou.ac.kr>
2023-11-08 23:02:58 +0900

    부분문자열 문제 풀이 완료

--- book/DFS/부분문자열.py
+++ book/DFS/부분문자열.py
@@ -1,15 +1,19 @@
 s1 = "ACBABCBAB"
 s2 = "CBBBAB"
 ans = -1e9
-def DFS(x, y, cnt, temp):
-    print(cnt, temp)
+def DFS(x, y, temp, cnt):
     global ans
     for i in range(x+1, len(s1)):
         for j in range(y+1, len(s2)):
             if s1[i] == s2[j]:
                 temp += s1[i]
-                DFS(i, j, cnt+1, temp)
+                DFS(i, j, temp, cnt+1)
+                temp = list(temp)
+                temp.pop()
+                temp = "".join(temp)
+                break
     if cnt > ans:
+        print(temp)
         ans = cnt
     return
 
@@ -18,7 +22,8 @@ def DFS(x, y, cnt, temp):
 for i in range(len(s1)):
     for j in range(len(s2)):
         if s1[i] == s2[j]:
-            DFS(i, j, 1, s1[i])
+            DFS(i, j, s1[i], 1)
+
 
 
 print(ans)
