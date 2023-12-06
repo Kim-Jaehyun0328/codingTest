527b4c10f07405973b2057b671885d3608a7abc7
KimJaehyun <rlawwogus@ajou.ac.kr>
2023-12-06 19:46:34 +0900

    호텔 대실

---
+++ programmers/level2/호텔 대실.py
@@ -0,0 +1,40 @@
+# def solution(book_time):
+#     last = []
+#     book_time.sort()
+
+#     for book in book_time:
+#         start_hour, start_minute = book[0].split(":")
+#         end_hour, end_minute = book[1].split(":")
+
+#         start = int(start_hour) * 60 + int(start_minute)
+#         end = int(end_hour) * 60 + int(end_minute)
+
+#         if last and min(last) <= start:
+#             last[last.index(min(last))] = end + 10
+#         else:
+#             last.append(end + 10) #10분 뒤 가능하니까
+
+
+#     return len(last)
+from heapq import heappop, heappush
+
+
+def solution(book_time):
+    answer = 1
+
+    # "HH:MM" → HH * 60 + MM
+    book_time_ref = [(int(s[:2]) * 60 + int(s[3:]), int(e[:2]) * 60 + int(e[3:])) for s, e in book_time]
+    book_time_ref.sort()
+
+    heap = []
+    for s, e in book_time_ref:
+        if not heap:
+            heappush(heap, e + 10)
+            continue
+        if heap[0] <= s:
+            heappop(heap)
+        else:
+            answer += 1
+        heappush(heap, e + 10)
+
+    return answer
