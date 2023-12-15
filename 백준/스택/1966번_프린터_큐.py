56dc0cd3ca50777499ad438bee330bd23e6c8b1e
KimJaehyun <rlawwogus@ajou.ac.kr>
2023-12-15 16:52:39 +0900

    deque, set 활용한 풀이 추가

--- 백준/스택/1966번_프린터_큐.py
+++ 백준/스택/1966번_프린터_큐.py
@@ -1,21 +1,44 @@
-import sys
+# import sys
+#
+# T = int(sys.stdin.readline())
+#
+# for _ in range(T):
+#     n, find = map(int, sys.stdin.readline().split())
+#     num = list(map(int, sys.stdin.readline().split()))
+#     idx = 0
+#     ans = 1
+#     while True:
+#         if num[idx] == max(num[idx:]):
+#             if idx % n == find:
+#                 print(ans)
+#                 break
+#             idx += 1
+#             ans += 1
+#             num.append(0)
+#         else:
+#             num.append(num[idx])
+#             idx += 1
 
-T = int(sys.stdin.readline())
 
-for _ in range(T):
-    n, find = map(int, sys.stdin.readline().split())
-    num = list(map(int, sys.stdin.readline().split()))
-    idx = 0
-    ans = 1
-    while True:
-        if num[idx] == max(num[idx:]):
-            if idx % n == find:
-                print(ans)
-                break
-            idx += 1
-            ans += 1
-            num.append(0)
-        else:
-            num.append(num[idx])
-            idx += 1
+from collections import deque
+n, find = 6, 0
+num = [1,1,9,1,1,1]
+idx = 0
+ans = 1
+pList = []
 
+for i in range(n):
+    pList.append((i, num[i]))
+
+pList = deque(pList)
+
+while True:
+    max_num = max([item[1] for item in pList])
+    if pList[0][1] == max_num:
+        if pList[0][0] == find:
+            print(ans)
+            break
+        pList.popleft()
+        ans += 1
+    else:
+        pList.append(pList.popleft())
