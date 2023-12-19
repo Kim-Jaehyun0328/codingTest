9eed879794cfdcdeb92624767906534efb734fba
KimJaehyun <rlawwogus@ajou.ac.kr>
2023-12-19 12:59:05 +0900

    큰 수의 법칙 최적화

--- book/그리디/큰 수의 법칙.py
+++ book/그리디/큰 수의 법칙.py
@@ -1,15 +1,50 @@
 n, m, k = 5, 8, 3
 data = [2, 4, 5, 4, 6]
 ans = 0
-data.sort(reverse= True)
+# data.sort(reverse= True)
+#
+# ch = 0
+# for i in range(m):
+#     if ch < k:
+#         ans += data[0]
+#         ch += 1
+#     else:
+#         ch = 0
+#         ans += data[1]
+#
+# print(ans)
 
-ch = 0
-for i in range(m):
-    if ch < k:
-        ans += data[0]
-        ch += 1
-    else:
-        ch = 0
-        ans += data[1]
+data.sort()
+first = data[-1]
+second = data[-2]
+
+cnt = m//(k+1)
+rest = m%(k+1)
+
+ans += ((first*k) + (second)) * cnt
+
+ans += first * rest
 
 print(ans)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
