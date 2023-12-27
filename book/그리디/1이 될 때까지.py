9e4015d06b21b009be06e496da346f7e5c959adb
KimJaehyun <rlawwogus@ajou.ac.kr>
2023-12-27 12:21:02 +0900

    1이 될 때까지

--- book/그리디/1이 될 때까지.py
+++ book/그리디/1이 될 때까지.py
@@ -1,13 +1,25 @@
-N, K = 25, 5
-answer = 0
+# N, K = 25, 5
+# answer = 0
+#
+#
+# while N != 1 and N > 1:
+#     if N%K == 0:
+#         N //= K
+#         answer += 1
+#     else:
+#         N -= 1
+#         answer += 1
+#
+# print(answer)
 
+n, k = 25, 3
+ans = 0
 
-while N != 1 and N > 1:
-    if N%K == 0:
-        N //= K
-        answer += 1
+while n != 1:
+    if n%k == 0:
+        n //= k
+        ans += 1
     else:
-        N -= 1
-        answer += 1
-
-print(answer)
+        n -= 1
+        ans += 1
+print(ans)
