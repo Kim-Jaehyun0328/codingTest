734f5489f959af961f8042d9b4c0cb1123e18f98
rlawwogus@ajou.ac.kr <rlawwogus@ajou.ac.kr>
2024-02-08 11:32:44 +0900

    게임개발

--- book/구현/게임 개발.py
+++ book/구현/게임 개발.py
@@ -47,56 +47,94 @@
 # 0: 북, 1: 동, 2: 남, 3:서
 # 방문을 했으면 매트릭스에서 2로 체크하기
 
-ans = 1
+# ans = 1
+#
+# N, M = 4, 4
+#
+# A, B, dir = 1, 1, 0
+#
+# nx = [-1,0,1,0]
+# ny = [0,-1,0,1]  # 북서남동 순으로 이동
+#
+# matrix = [[1,1,1,1],[1,0,0,1],[1,1,0,1],[1,1,1,1]]
+# matrix[A][B] = 2
+#
+# def turn_left(dir):
+#     if dir + 1 > 3: #북쪽에서 왼쪽 방향으로 회전할때는 서쪽이 된다.
+#         return 0
+#     else:
+#         return dir + 1
+#
+#
+# while True:
+#     cnt = 0 #상하좌우 다 돌았는지 체크
+#     for _ in range(4):
+#         dir = turn_left(dir)
+#         dx = nx[dir] + A
+#         dy = ny[dir] + B
+#         if 0 <= dx < N and 0 <= dy < M and matrix[dx][dy] == 0:  # 갈 수 있는 곳이라면
+#             A, B = dx, dy
+#             matrix[dx][dy] = 2  # 간 곳으로 체크
+#             ans += 1
+#             break
+#         else:
+#             cnt += 1
+#     if cnt == 4: #상하좌우 모두 갈 곳이 없다면 뒤로 가야 함
+#         dx = nx[(dir+2)%4] + A
+#         dy = ny[(dir+2)%4] + B
+#         if 0<=dx<N and 0<=dy<M and matrix[dx][dy] != 1: # 갈 수 있다면
+#             A, B = dx, dy
+#         else:
+#             break
+#
+#
+#
+# print(ans)
 
-N, M = 4, 4
-
-A, B, dir = 1, 1, 0
-
-nx = [-1,0,1,0]
-ny = [0,-1,0,1]  # 북서남동 순으로 이동
-
-matrix = [[1,1,1,1],[1,0,0,1],[1,1,0,1],[1,1,1,1]]
-matrix[A][B] = 2
-
-def turn_left(dir):
-    if dir + 1 > 3: #북쪽에서 왼쪽 방향으로 회전할때는 서쪽이 된다.
-        return 0
-    else:
-        return dir + 1
 
 
+def turn_left(direction):
+    direction -= 1
+    if direction < 0:
+        direction = 3
+    return direction
+
+answer = 1
+x, y, direction = 1, 1, 0
+arr = [[1,1,1,1], [1,0,0,1], [1,1,0,1], [1,1,1,1]]
+length = len(arr)
+arr[x][y] = 2 #현재 위치는 방문한 곳으로 체크하기
+steps = [(-1,0), (0,1), (1,0), (0,-1)]
+
+cnt = 0
 while True:
-    cnt = 0 #상하좌우 다 돌았는지 체크
-    for _ in range(4):
-        dir = turn_left(dir)
-        dx = nx[dir] + A
-        dy = ny[dir] + B
-        if 0 <= dx < N and 0 <= dy < M and matrix[dx][dy] == 0:  # 갈 수 있는 곳이라면
-            A, B = dx, dy
-            matrix[dx][dy] = 2  # 간 곳으로 체크
-            ans += 1
-            break
+    direction = turn_left(direction)
+
+    nx = x + steps[direction][0]
+    ny = y + steps[direction][1]
+    if 0<=nx<length and 0<=ny<length and arr[nx][ny] == 0:
+        arr[nx][ny] = 2 #간 곳으로 체크
+        x, y = nx, ny
+        answer += 1
+        cnt = 0
+        continue
+    else:
+        cnt +=1
+
+    if cnt == 4: #4방향 모두 갈 곳이 없다면
+        nx = x - steps[direction][0] #바라보는 방향 반대로 가야하기 때문에 "빼기"를 한다.
+        ny = y - steps[direction][1]
+        if 0<=nx<length and 0<=ny<length and arr[nx][ny] == 0:
+            x = nx
+            y = ny
+            cnt = 0
         else:
-            cnt += 1
-    if cnt == 4: #상하좌우 모두 갈 곳이 없다면 뒤로 가야 함
-        dx = nx[(dir+2)%4] + A
-        dy = ny[(dir+2)%4] + B
-        if 0<=dx<N and 0<=dy<M and matrix[dx][dy] != 1: # 갈 수 있다면
-            A, B = dx, dy
-        else:
             break
 
 
 
-print(ans)
 
-
-
-
-
-
-
+print(answer)
 
 
 
