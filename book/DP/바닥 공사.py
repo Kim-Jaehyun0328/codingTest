33ae877d3dad5ae540f614b0d0f36946d2ca8a13
rlawwogus@ajou.ac.kr <rlawwogus@ajou.ac.kr>
2024-03-04 17:38:12 +0900

    바닥 공사

--- book/DP/바닥 공사.py
+++ book/DP/바닥 공사.py
@@ -1,11 +1,41 @@
-n, m = 2, 100
+# n, m = 2, 100
+#
+# dy = [0 for _ in range(1001)]
+#
+# dy[1] = 1
+# dy[2] = 3
+#
+# for i in range(3, 1001):
+#     dy[i] = (dy[i-1] + 2*dy[i-2])%796796
+#
+# print(dy[m])
 
+n = 3
 dy = [0 for _ in range(1001)]
 
 dy[1] = 1
 dy[2] = 3
 
-for i in range(3, 1001):
-    dy[i] = (dy[i-1] + 2*dy[i-2])%796796
+for i in range(3, n+1):
+    dy[i] = (dy[i-1] + dy[i-2] * 2) % 769769
 
-print(dy[m])
+print(dy[n])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
