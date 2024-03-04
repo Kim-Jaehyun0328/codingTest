d1e2eaae5539a0e891d4c8e072ed6166e1239a7c
rlawwogus@ajou.ac.kr <rlawwogus@ajou.ac.kr>
2024-03-04 17:30:38 +0900

    개미 전사

--- book/DP/개미 전사.py
+++ book/DP/개미 전사.py
@@ -1,9 +1,41 @@
+# n = 4
+# arr = [1, 3, 1, 5]
+# dy = [0 for _ in range(n)]
+# dy[0] = arr[0]
+# dy[1] = max(arr[0], arr[1])
+#
+# for i in range(2, n):
+#     dy[i] = max(dy[i - 1], dy[i - 2] + arr[i])
+# print(dy[n - 1])
+
 n = 4
-arr = [1, 3, 1, 5]
-dy = [0 for _ in range(n)]
+arr = [1,3,1,5]
+dy = [0 for _ in range(101)]
+
 dy[0] = arr[0]
 dy[1] = max(arr[0], arr[1])
 
-for i in range(2, n):
-    dy[i] = max(dy[i - 1], dy[i - 2] + arr[i])
-print(dy[n - 1])
+for i in range(2, len(arr)):
+    dy[i] = max(dy[i-1], dy[i-2] + arr[i])
+
+print(dy[n-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
