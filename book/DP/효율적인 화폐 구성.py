c4555b762a6b6bbe9e24756148cb0308c294c6d4
rlawwogus@ajou.ac.kr <rlawwogus@ajou.ac.kr>
2024-03-04 17:57:40 +0900

    효율적인 화폐 구성

--- book/DP/효율적인 화폐 구성.py
+++ book/DP/효율적인 화폐 구성.py
@@ -1,18 +1,54 @@
-n = 3
-m = 7
-arr = [2, 3, 5]
-dy = [10001 for _ in range(m + 1)]
+# n = 3
+# m = 7
+# arr = [2, 3, 5]
+# dy = [10001 for _ in range(m + 1)]
+#
+# for x in arr:
+#     dy[x] = 1
+#
+# for i in range(n):
+#     for j in range(arr[i], m+1):
+#         dy[j] = min(dy[j], dy[j-arr[i]]+1)
+#
+# if dy[m] == 10001:
+#     print(-1)
+# else:
+#     print(dy[m])
+#
+# print(dy)
 
-for x in arr:
-    dy[x] = 1
+
+n, m = 2, 15
+coins = [2, 3]
+
+dy = [10001 for _ in range(m+1)]
+dy[0] = 0
+for coin in coins:
+    dy[coin] = 1
 
 for i in range(n):
-    for j in range(arr[i], m+1):
-        dy[j] = min(dy[j], dy[j-arr[i]]+1)
+    for j in range(coins[i], m+1):
+        dy[j] = min(dy[j], dy[j-coins[i]]+1)
+
 
 if dy[m] == 10001:
     print(-1)
 else:
     print(dy[m])
 
-print(dy)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
