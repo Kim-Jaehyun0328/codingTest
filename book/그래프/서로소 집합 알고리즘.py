96592b3ae09d606a2ee6a34bb521074e5451b39a
rlawwogus@ajou.ac.kr <rlawwogus@ajou.ac.kr>
2024-03-07 09:53:49 +0900

    서로소 집합 알고리즘 기본

---
+++ book/그래프/서로소 집합 알고리즘.py
@@ -0,0 +1,33 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        return find_parent(parent, parent[x])
+    return x
+
+def unioin_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+v, e = 6, 4
+arr = [(1,4),(2,3),(2,4),(5,6)]
+parent = [0 for _ in range(v+1)]
+
+for i in range(1, v+1):
+    parent[i] = i
+
+for a, b in arr:
+    unioin_parent(parent, a, b)
+
+
+print('각 원소가 속한 집합: ', end= " ")
+for i in range(1, v+1):
+    print(find_parent(parent, i), end= " ")
+
+print()
+
+print("부모 테이블: ", end= " ")
+for i in range(1, v+1):
+    print(parent[i], end= " ")
