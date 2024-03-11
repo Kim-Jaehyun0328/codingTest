804f7de4b6e5c15c07adea7e829dd228110fc5a7
rlawwogus@ajou.ac.kr <rlawwogus@ajou.ac.kr>
2024-03-11 09:57:35 +0900

    개선된 서로소 집합 알고리즘

--- book/그래프/서로소 집합 알고리즘.py
+++ book/그래프/서로소 집합 알고리즘.py
@@ -1,7 +1,48 @@
+# def find_parent(parent, x):
+#     if parent[x] != x:
+#         return find_parent(parent, parent[x])
+#     return x
+#
+# def unioin_parent(parent, a, b):
+#     a = find_parent(parent, a)
+#     b = find_parent(parent, b)
+#     if a < b:
+#         parent[b] = a
+#     else:
+#         parent[a] = b
+#
+# v, e = 6, 4
+# arr = [(1,4),(2,3),(2,4),(5,6)]
+# parent = [0 for _ in range(v+1)]
+#
+# for i in range(1, v+1):
+#     parent[i] = i
+#
+# for a, b in arr:
+#     unioin_parent(parent, a, b)
+#
+#
+# print('각 원소가 속한 집합: ', end= " ")
+# for i in range(1, v+1):
+#     print(find_parent(parent, i), end= " ")
+#
+# print()
+#
+# print("부모 테이블: ", end= " ")
+# for i in range(1, v+1):
+#     print(parent[i], end= " ")
+
+
+
 def find_parent(parent, x):
     if parent[x] != x:
-        return find_parent(parent, parent[x])
-    return x
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+# def find_parent(parent, x):
+#     if parent[x] != x:
+#         return find_parent(parent, parent[x])
+#     return x
 
 def unioin_parent(parent, a, b):
     a = find_parent(parent, a)
@@ -18,7 +59,12 @@ parent = [0 for _ in range(v+1)]
 for i in range(1, v+1):
     parent[i] = i
 
+
+cycle = False
 for a, b in arr:
+    if find_parent(parent, parent[a]) == find_parent(parent, parent[b]):
+        cycle = True
+        break
     unioin_parent(parent, a, b)
 
 
