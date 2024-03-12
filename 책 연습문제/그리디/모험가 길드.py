bfcb3a04e66d8255c9a1625b198bd387d55b6b36
rlawwogus@ajou.ac.kr <rlawwogus@ajou.ac.kr>
2024-03-12 09:51:51 +0900

    모험가 길드

--- 책 연습문제/그리디/모험가 길드.py
+++ 책 연습문제/그리디/모험가 길드.py
@@ -1,15 +1,62 @@
-ans = 0
-n = 5
-arr = [2, 3, 1, 2, 2]
+# ans = 0
+# n = 5
+# arr = [2, 3, 1, 2, 2]
+#
+# arr.sort(reverse=True)
+#
+#
+# cnt = 0
+# for i in range(len(arr)):
+#     if cnt == 0:
+#         ans += 1
+#         cnt = arr[i]
+#     cnt -= 1
+#
+# print(ans)
 
-arr.sort(reverse=True)
 
+N = 5
+arr = [2,3,1,2,2]
+answer = 0
+arr.sort()
 
 cnt = 0
-for i in range(len(arr)):
-    if cnt == 0:
-        ans += 1
-        cnt = arr[i]
-    cnt -= 1
+for a in arr:
+    cnt += 1
+    if a <= cnt:
+        answer += 1
+        cnt = 0
 
-print(ans)
+print(answer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
