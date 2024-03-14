05649d0a28a32db836fc742d618fc35804ed5d4e
rlawwogus@ajou.ac.kr <rlawwogus@ajou.ac.kr>
2024-03-14 11:05:36 +0900

    만들 수 없는 금액

--- 책 연습문제/그리디/만들 수 없는 금액.py
+++ 책 연습문제/그리디/만들 수 없는 금액.py
@@ -1,13 +1,46 @@
-n = 5
-arr = [3, 2, 1, 1, 9]
-arr.sort(reverse=True)
+# n = 5
+# arr = [3, 2, 1, 1, 9]
+# arr.sort(reverse=True)
+#
+#
+# for i in range(1, sum(arr)):
+#     sum = 0
+#     for x in arr:
+#         if sum+x <= i:
+#             sum += x
+#     if sum != i:
+#         print(i)
+#         break
 
 
-for i in range(1, sum(arr)):
-    sum = 0
-    for x in arr:
-        if sum+x <= i:
-            sum += x
-    if sum != i:
-        print(i)
+
+N = 5
+arr = [3,2,1,1,9]
+arr.sort()
+
+target = 1
+
+for i in arr:
+    if target < i:
         break
+    target += i
+
+print(target)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
