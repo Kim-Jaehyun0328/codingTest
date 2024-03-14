d051f2c9dddcef888e163cf3853b9428f406a338
rlawwogus@ajou.ac.kr <rlawwogus@ajou.ac.kr>
2024-03-14 09:57:13 +0900

    곱하기 혹은 더하기

--- 책 연습문제/그리디/곱하기 혹은 더하기.py
+++ 책 연습문제/그리디/곱하기 혹은 더하기.py
@@ -1,12 +1,56 @@
-s = "567"
+# s = "567"
+#
+# ans = int(s[0])
+#
+# for x in s[1:]:
+#     mul = ans * int(x)
+#     plus = ans + int(x)
+#     temp = max(mul, plus)
+#     if temp <= 2000000000:
+#         ans = temp
+#
+# print(ans)
 
-ans = int(s[0])
 
-for x in s[1:]:
-    mul = ans * int(x)
-    plus = ans + int(x)
-    temp = max(mul, plus)
-    if temp <= 2000000000:
-        ans = temp
+S = "567"
+
+ans = int(S[0])
+
+for i in S[1:]:
+    ans = max(ans + int(i), ans * int(i))
 
 print(ans)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
