26f5bcebaddb6c9ca0e3a5649600c7a10696072a
rlawwogus@ajou.ac.kr <rlawwogus@ajou.ac.kr>
2024-03-14 10:07:54 +0900

    문자열 뒤집기

--- 책 연습문제/그리디/문자열 뒤집기.py
+++ 책 연습문제/그리디/문자열 뒤집기.py
@@ -1,19 +1,58 @@
-s = "0001100"
+# s = "0001100"
+#
+# arr_0 = 0
+# arr_1 = 0
+#
+# temp = s[0]
+# for x in s[1:]:
+#     if x != temp:
+#         if temp == "0":
+#             arr_0 += 1
+#         else:
+#             arr_1 += 1
+#         temp = x
+# if temp == "0":
+#     arr_0 += 1
+# else:
+#     arr_1 += 1
+#
+# print(min(arr_0, arr_1))
 
-arr_0 = 0
-arr_1 = 0
 
-temp = s[0]
-for x in s[1:]:
-    if x != temp:
+
+S = "0001100"
+
+zero_cnt = 0
+one_cnt = 0
+temp = S[0]
+for i in S[1:]:
+    if temp != i: #바뀜
         if temp == "0":
-            arr_0 += 1
+            zero_cnt += 1
         else:
-            arr_1 += 1
-        temp = x
-if temp == "0":
-    arr_0 += 1
-else:
-    arr_1 += 1
+            one_cnt += 1
+        temp = i
 
-print(min(arr_0, arr_1))
+print(min(zero_cnt, one_cnt))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
