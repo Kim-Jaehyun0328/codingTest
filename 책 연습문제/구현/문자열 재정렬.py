b6f687fee0706805be27b144dd7e93709224ac99
rlawwogus@ajou.ac.kr <rlawwogus@ajou.ac.kr>
2024-03-15 09:42:26 +0900

    문자열 재정렬

--- 책 연습문제/구현/문자열 재정렬.py
+++ 책 연습문제/구현/문자열 재정렬.py
@@ -1,12 +1,58 @@
-s = "AJKDLSI412K4JSJ9D"
+# s = "AJKDLSI412K4JSJ9D"
+# num = 0
+# ans = []
+# for x in s:
+#     if x.isdigit():
+#         num += int(x)
+#     else:
+#         ans.append(x)
+# ans.sort()
+# ans.append(str(num))
+# ans = "".join(ans)
+# print(ans)
+
+S = "K1KA5CB7"
+
+temp = []
 num = 0
-ans = []
-for x in s:
-    if x.isdigit():
-        num += int(x)
+for i in S:
+    if i.isalpha():
+        temp.append(i)
     else:
-        ans.append(x)
-ans.sort()
-ans.append(str(num))
-ans = "".join(ans)
-print(ans)
+        num += int(i)
+
+temp.sort()
+temp = "".join(temp)
+
+
+if num != 0:
+    temp += str(num)
+
+print(temp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
