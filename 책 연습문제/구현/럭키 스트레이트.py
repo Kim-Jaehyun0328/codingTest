c8b1db1d9d48f83350272a3e305a3458b2098d8f
rlawwogus@ajou.ac.kr <rlawwogus@ajou.ac.kr>
2024-03-15 09:36:37 +0900

    럭키 스트레이트

--- 책 연습문제/구현/럭키 스트레이트.py
+++ 책 연습문제/구현/럭키 스트레이트.py
@@ -1,14 +1,65 @@
-s = "123411"
-half = len(s) // 2
+# s = "123411"
+# half = len(s) // 2
+#
+# sum = 0
+# for i in range(half):
+#     sum += int(s[i])
+#
+# for x in s[half:]:
+#     sum -= int(x)
+#
+# if sum == 0:
+#     print("LUCKY")
+# else:
+#     print("READY")
 
-sum = 0
-for i in range(half):
-    sum += int(s[i])
 
-for x in s[half:]:
-    sum -= int(x)
+N = "123402"
 
-if sum == 0:
+half = len(N)//2
+
+
+
+first = sum(int(i) for i in N[:half])
+second = sum(int(i) for i in N[half:])
+
+
+if first == second:
     print("LUCKY")
 else:
     print("READY")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
