3cb6499b6ffe434663ace20e8ba58fbb8164e54b
JaeniorDeveloper <108642772+Kim-Jaehyun0328@users.noreply.github.com>
2025-09-11 11:49:16 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 936 ms, Memory: 45732 KB -BaekjoonHub

---
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int N, K;
+    static boolean[] visited = new boolean[100001];
+    static int[][] ch = new int[100001][2];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        for(int i=0; i<=100000; i++) {
+            ch[i][0] = Integer.MAX_VALUE;
+        }
+        visited[N] = true;
+        ch[N][0] = 0;
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(N);
+
+        while(!q.isEmpty()) {
+            int pos = q.poll();
+
+            if(pos==K) {
+                System.out.println(ch[pos][0]);
+                Stack<Integer> stack = new Stack<>();
+                int temp = K;
+                while (temp != N) {
+                    stack.push(temp);
+                    temp = ch[temp][1];
+                }
+                stack.push(N);
+
+                while (!stack.isEmpty()) {
+                    System.out.print(stack.pop() + " ");
+                }
+            }
+
+            if(pos-1 > -1 && !visited[pos-1]) {
+                ch[pos-1][0] = ch[pos][0] +1;
+                ch[pos-1][1] = pos;
+                visited[pos-1] = true;
+                q.offer(pos-1);
+            }
+            if(pos + 1 < 100001 && !visited[pos+1]) {
+                ch[pos+1][0] = ch[pos][0] +1;
+                ch[pos+1][1] = pos;
+                visited[pos+1] = true;
+                q.offer(pos+1);
+            }
+            if(pos*2 < 100001 && !visited[pos*2]) {
+                ch[pos*2][0] = ch[pos][0] +1;
+                ch[pos*2][1] = pos;
+                visited[pos*2] = true;
+                q.offer(pos*2);
+            }
+        }
+    }
+}
