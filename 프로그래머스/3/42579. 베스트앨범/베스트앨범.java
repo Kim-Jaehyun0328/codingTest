aeb774c57793eda05c4c4e81cd29884d0f8bb9c7
JaeniorDeveloper <108642772+Kim-Jaehyun0328@users.noreply.github.com>
2025-10-01 15:33:30 +0900

    [level 3] Title: 베스트앨범, Time: 1.44 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Type implements Comparable<Type>{
+    int index;
+    int plays;
+    
+    Type(int a, int b) {
+        index=a; plays=b;
+    }
+    @Override
+    public int compareTo(Type o) {
+        if(o.plays==this.plays) return this.index-o.index;
+        return o.plays-this.plays;
+    }
+}
+
+class Solution {
+    public int[] solution(String[] genres, int[] plays) {
+        int[] answer = {};
+        HashMap<String, ArrayList<Type>> map = new HashMap<>();
+        HashMap<String, Integer> most = new HashMap<>();
+        
+        //most, map에 값 추가
+        for(int i=0; i<genres.length; i++) {
+            most.put(genres[i],most.getOrDefault(genres[i],0) + plays[i]);
+            map.putIfAbsent(genres[i], new ArrayList<>());
+            map.get(genres[i]).add(new Type(i, plays[i]));
+        }
+        
+        
+        ArrayList<Integer> temp = new ArrayList<>();
+        ArrayList<Map.Entry<String, Integer>> genreList = new ArrayList<>(most.entrySet());
+        genreList.sort((a, b) -> b.getValue() - a.getValue());
+        
+        for(Map.Entry<String, Integer> entry : genreList) {
+            String key = entry.getKey();
+            ArrayList<Type> arr = map.get(key);
+            Collections.sort(map.get(key));
+            temp.add(arr.get(0).index);
+            if(arr.size()>1) temp.add(arr.get(1).index);
+        }
+        answer = new int[temp.size()];
+        for(int i=0; i<temp.size(); i++) {
+            answer[i] = temp.get(i);
+        }
+        
+        return answer;
+    }
+}
