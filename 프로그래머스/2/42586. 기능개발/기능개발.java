1a536439a75cbe20df8ac901aa35492bf888fbba
JaeniorDeveloper <108642772+Kim-Jaehyun0328@users.noreply.github.com>
2025-10-02 15:42:20 +0900

    [level 2] Title: 기능개발, Time: 3.23 ms, Memory: 86.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        ArrayList<Integer> arr = new ArrayList<>();
+        Deque<Integer> dq = new ArrayDeque<>();
+        
+        for(int i=0; i<progresses.length; i++) {
+            int day = 0;
+            while(progresses[i]<100) {
+                progresses[i] += speeds[i];
+                day++;
+            }
+            dq.offer(day);
+        }
+        
+        int temp = dq.poll();
+        int cnt = 1;
+        while(!dq.isEmpty()) {
+            int num = dq.poll();
+            if(num > temp) {
+                temp = num;
+                arr.add(cnt);
+                cnt = 1;
+            } else cnt++;
+        }
+        arr.add(cnt);
+        
+        for(int n : arr) {
+            System.out.println(n);
+        }
+        
+        answer = arr.stream().mapToInt(i->i).toArray();
+        return answer;
+    }
+}
