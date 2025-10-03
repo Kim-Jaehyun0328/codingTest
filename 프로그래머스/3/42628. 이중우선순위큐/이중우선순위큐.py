dc3cd3ed8d254d99cb42bd08df53ac77acbbc23d
JaeniorDeveloper <108642772+Kim-Jaehyun0328@users.noreply.github.com>
2025-10-03 16:34:10 +0900

    [level 3] Title: 이중우선순위큐, Time: 712.41 ms, Memory: 15.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -0,0 +1,23 @@
+def solution(operations):
+    temp = []
+    
+    for x in operations:
+        if x[0] == "I" :
+            if x[2].isdigit(): #정수라면
+                temp.append(int(x[2:]))
+            else:
+                temp.append((-1)*int(x[3:]))
+        else:
+            if x[2] == "1" and temp: #우선순위 큐가 비어있지 않고, "D 1" 이라면
+                temp.remove(max(temp))
+            elif x[2] == "-" and temp:
+                temp.remove(min(temp))
+                
+        
+    
+        
+        
+    if not temp:
+        return [0, 0]
+    else:
+        return [max(temp), min(temp)]
